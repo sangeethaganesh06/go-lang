72cb3f13ab7919eda3263d9f17b065dc36375e07
sangeethaganesh06 <127288865+sangeethaganesh06@users.noreply.github.com>
2023-03-14 10:19:24 +0530

    rangeupdate.go

--- range.go
+++ range.go
@@ -6,10 +6,10 @@ for i,n:=range arr{ 					// range using array
 fmt.Printf("arr[%d]:%d\n",i,n)
 }
 slc:=[]int{111,222,33}
-for i1,n1:=range slc{ 					// range using slic e
+for i1,n1:=range slc{ 					// range using slice
 fmt.Printf("slice[%d]:%d\n",i1,n1)
 }
 str:="sangeethamathigomathiganesh"
-for i,n:=range arr{ 					// range using array
-fmt.Printf("arr[%d]:%d\n",i,n)
+for i,n:=range str{ 					// range using string
+fmt.Printf("string[%d]:%d\n",i,n)
 }
